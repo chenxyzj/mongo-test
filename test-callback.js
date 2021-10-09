aa42a633bf941d9456a0e7913bff5eab12f99709
chenxyzj <chenxyzj@163.com>
2021-10-09 13:58:39 +0800

    删除回调函数内的db和collection赋值。

--- test-callback.js
+++ test-callback.js
@@ -15,22 +15,13 @@ client.connect(() => {
     console.log('create collection successfully.');
     collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }],(err,result) => {
         console.log('Inserted results is:',result);
-        const db = client.db(dbName);
-        console.log('open db successfully.');
-        const collection = db.collection('documents');
-        console.log('open collection successfully.');        
+     
         collection.find({}).toArray((err,findResult) => {
             console.log('Found documents =>', findResult);
-            const db = client.db(dbName);
-            console.log('open db successfully.');
-            const collection = db.collection('documents');
-            console.log('open collection successfully.');        
+       
             collection.deleteMany({}, (err,result) => {
                 console.log("Deleted documents:", result);
-                const db = client.db(dbName);
-                console.log('open db successfully.');
-                const collection = db.collection('documents');
-                console.log('open collection successfully.');        
+       
                 collection.find({}).toArray((err,findResult) => {
                     console.log('Found docments is:',findResult);
                     client.close();
