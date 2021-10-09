6c77364c5315dce144770b73da29f91bed7e62e8
chenxyzj <chenxyzj@163.com>
2021-10-09 10:24:05 +0800

    代码可以执行，但数据库连接没有关闭。promise的then嵌套不推荐。

---
+++ test.js
@@ -0,0 +1,21 @@
+const {MongoClient} = require('mongodb');
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+const dbName = 'myProject';
+
+client.connect()
+    .then(() => {
+        console.log('Connected to mongodb successfully.');
+        const db = client.db(dbName);
+        console.log('create db successfully.');
+        const collection = db.collection('documents');
+        console.log('create collection successfully.');
+        collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }])
+        .then((result) => console.log('Result is:',result))
+        .catch((err) =>  console.log(err));
+    })
+    .then((result) => console.log('Result is:',result))
+    // .then(() => client.close())
+    .catch((err) =>  console.log(err));
