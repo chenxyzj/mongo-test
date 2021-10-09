c7b1ef4dd848c69857335dc54335085652a11bf4
chenxyzj <chenxyzj@163.com>
2021-10-09 14:05:32 +0800

    增加简单的出错处理。

--- test-callback.js
+++ test-callback.js
@@ -5,22 +5,31 @@ const client = new MongoClient(url);
 
 const dbName = 'myProject';
 
-client.connect(() => {
+client.connect((err) => {
+    if(err) return console.log('Error:',err);
     console.log('Connected to mongodb successfully.');
     const db = client.db(dbName);
     console.log('create db successfully.');
     const collection = db.collection('documents');
     console.log('create collection successfully.');
     collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }],(err,result) => {
+        if(err) return console.log(err);
+
         console.log('Inserted results is:',result);
      
         collection.find({}).toArray((err,findResult) => {
+            if(err) return console.log(err);
+
             console.log('Found documents =>', findResult);
        
             collection.deleteMany({}, (err,result) => {
+                if(err) return console.log(err);
+
                 console.log("Deleted documents:", result);
        
                 collection.find({}).toArray((err,findResult) => {
+                    if(err) return console.log(err);
+
                     console.log('Found docments is:',findResult);
                     client.close();
                 });
